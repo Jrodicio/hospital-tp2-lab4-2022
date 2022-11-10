7c95089ee5eba30519e184e9b29c3199d7fc351e
Julian <60717455+Jrodicio@users.noreply.github.com>
2022-11-10 19:54:57 -0300

    Se suben correcciones sobre sprint 3

--- src/app/modules/solicitud-turnos/components/listado-pacientes/listado-pacientes.component.ts
+++ src/app/modules/solicitud-turnos/components/listado-pacientes/listado-pacientes.component.ts
@@ -13,7 +13,7 @@ export class ListadoPacientesComponent implements OnInit {
 
   public listaPacientes: any[] = [];
   public filtroPacientes: string = '';
-
+  public cargando: boolean = false;
   public pacienteElegido: any = {uid: '0'}
 
 
@@ -21,7 +21,12 @@ export class ListadoPacientesComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.firestoreService.getDocuments('users').subscribe(users => {this.listaPacientes = users.filter(usuario => usuario['perfil'] == 'Paciente')});
+    this.cargando = true;
+    this.firestoreService.getDocuments('users').subscribe(users => {
+      this.listaPacientes = users.filter(usuario => usuario['perfil'] == 'Paciente');
+      this.cargando = false;
+
+    });
   }
 
   seleccionar(paciente: any){
